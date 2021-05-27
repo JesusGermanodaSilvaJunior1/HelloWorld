2b8cc3077212b483ddb2c4610e18ca715aa57f64
JesusGermanodaSilvaJunior1 <77981613+JesusGermanodaSilvaJunior1@users.noreply.github.com>
2021-05-27 19:36:58 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+            // Int - 156 como 32334
+            // Float - 15,6 como 232.132
+            // Bool - true ou false
+            // String - " texto"
+            // Char - 'caracter'
+
+            // A-Z
+            // 0 - 9
+            // _
+            // não pode usar palavras reservadas
+        /*
+        int segundaGuerraMundial = 1942;
+        string cor_favorita = "azul";
+        float velocidadeF1 = 294.48f;
+        bool segundaGuerraMundialAconteceu = true;
+        {
+            Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Sou um desenvolvedor");
+            Console.WriteLine("segundaGuerraMundial");
+            Console.WriteLine("cor_favorita");
+            Console.WriteLine("velocidadeF1");
+            Console.WriteLine("segundaGuerramundialAconteceu");
+            Console.ReadLine();
+        */
+
+        }
+    }
+}
